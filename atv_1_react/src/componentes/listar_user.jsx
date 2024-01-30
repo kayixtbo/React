5c52b0c1b22408623f0a3908a6cbb7380ff297a3
Kaylany Luiza Vicente Oliveira Silva <140958751+kayixtbo@users.noreply.github.com>
2024-01-30 04:17:23 +0000

    correção de bugs

--- atv_1_react/src/componentes/listar_user.jsx
+++ atv_1_react/src/componentes/listar_user.jsx
@@ -1,21 +1,21 @@
-import { useState} from 'react'
+import React, { useState} from 'react'
 import { users } from './users'
 
 export const User_dados = ()=>{
     const [number, setNumber] = useState(0)
     const [person, setPerson] = useState(users[number])
-
+    
     const change_user = ()=>{
-        if (number>=3){
-            setNumber(0)
+        if (number>=users.length-1){
+            setNumber(number-number)
         }else{
             setNumber(number+1)
         }
-
         setPerson(users[number])
-        
+        console.log(number);
+        console.log(person);
     }
-
+    
     return(<div>
             <header>
                 <h1>olá, meu nome é {person.nome}!</h1>
--- atv_2_react/src/routes/get_person.jsx
+++ atv_2_react/src/routes/get_person.jsx
@@ -3,17 +3,19 @@ import { useState } from 'react'
 
 export function Person(){
     const [id,setId] = useState(0)
-    const [person,setPerson] = useState()
-
-    let path = `https://rickandmortyapi.com/api/character/${id}`
-
+    const [count, setCount] = useState(0)
+    const [person,setPerson] = useState({})
+    
+    let path = `https://rickandmortyapi.com/api/character`
+    
     axios.get(path).then((response) => {
-    const personagem = response;
-    setPerson(personagem);
+    const personagens = response.data.results
+    setPerson(personagens[id]);
+    setCount(personagens.length)
   });
 
     const change_id_more = ()=>{
-        if(id>826){
+        if(id>count-1){
             setId(0)
         }else{
             setId(id+1)
@@ -21,14 +23,12 @@ export function Person(){
     }
 
     const change_id_less = ()=>{
-        if(id<0){
-            setId(826)
+        if(id===0){
+            setId(count-1)
         }else{
             setId(id-1)
         }
     }
-
-    
 
     return (
         <div>
@@ -37,8 +37,8 @@ export function Person(){
             </header>
             <main>
                 <div>
-                    <p>Status: {person.name}</p>
-                    <p>Specie: {person.specie}</p>
+                    <p>Status: {person.status}</p>
+                    <p>Specie: {person.species}</p>
                     <p>Tipo: {person.type}</p>
                     <p>Genero: {person.gender}</p>
                 </div>
